0fa8c4cb4f617cec84d3b5dbfe637b2d583fc4cc
isidak <isidak@gmail.com>
2024-03-21 00:17:03 +0100

    Auto-generated commit

---
+++ chunk-NY6VIIEW.js
@@ -0,0 +1,1 @@
+import{Hb as r,Pb as p,pb as o,qb as a,va as i}from"./chunk-FJN2S7US.js";var c=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-file-input"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,f){n&1&&(o(0,"p"),r(1,"file-input works!"),a())}});let t=e;return t})();export{c as FileInputComponent};
