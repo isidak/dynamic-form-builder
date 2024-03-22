af9ca47144733a62e1812d842e7730ed5d4c98a5
isidak <isidak@gmail.com>
2024-03-22 13:27:46 +0100

    tooltips added

--- src/app/features/edit-component-wrapper/edit-component-wrapper.component.ts
+++ src/app/features/edit-component-wrapper/edit-component-wrapper.component.ts
@@ -1,10 +1,11 @@
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-edit-component-wrapper',
   standalone: true,
-  imports: [CdkDrag, CdkDragHandle],
+  imports: [CdkDrag, CdkDragHandle, NgbTooltipModule],
   templateUrl: './edit-component-wrapper.component.html',
   styleUrl: './edit-component-wrapper.component.css'
 })
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,10 +1,11 @@
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CdkDrag, CdkDragHandle],
+  imports: [CdkDrag, CdkDragHandle, NgbTooltipModule],
   templateUrl: './card.component.html',
 })
 export class CardComponent {
